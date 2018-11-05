16b2d11b03a9a93b6770f4aa5625cae93e33b00b
Rommel Rico <me@rommelrico.com>
2018-11-04 18:06:20 -0800

    Indentation and styling

--- app/src/main/java/com/rommelrico/androidlistviewdemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidlistviewdemo/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.rommelrico.androidlistviewdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
@@ -16,10 +15,17 @@ class MainActivity : AppCompatActivity() {
 
         val friendsListView = findViewById<ListView>(R.id.friendListView)
         val myFriends = arrayOf("Mark", "Jane", "Susan", "Jan")
-        var arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myFriends)
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myFriends)
         friendsListView.adapter = arrayAdapter
 
-        friendsListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id -> Toast.makeText(applicationContext, "Hello ${myFriends.get(position)}", Toast.LENGTH_LONG).show() }
+        friendsListView.onItemClickListener =
+                AdapterView.OnItemClickListener { parent, view, position, id ->
+                    Toast.makeText(
+                            applicationContext,
+                            "Hello ${myFriends.get(position)}",
+                            Toast.LENGTH_LONG
+                    ).show()
+                }
 
     } // end onCreate.
 }
