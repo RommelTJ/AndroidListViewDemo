6f2a4b1dfeeefb1110013a4aa06abfcdaa3501f3
Rommel Rico <me@rommelrico.com>
2018-11-04 18:01:08 -0800

    setOnItemClickListener

--- app/src/main/java/com/rommelrico/androidlistviewdemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidlistviewdemo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rommelrico.androidlistviewdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
@@ -15,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         val myFriends = arrayOf("Mark", "Jane", "Susan", "Jan")
         var arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myFriends)
         friendsListView.adapter = arrayAdapter
+
+        friendsListView.setOnItemClickListener(object: AdapterView.OnItemClickListener() {
+            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
     }
 }
