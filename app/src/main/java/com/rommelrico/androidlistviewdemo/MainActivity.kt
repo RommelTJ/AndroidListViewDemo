c8c57f0bee099d636facaceba3ab584c8e4ae09c
Rommel Rico <me@rommelrico.com>
2018-11-04 17:59:02 -0800

    Setting up an arrayAdapter with a list of friends

--- app/src/main/java/com/rommelrico/androidlistviewdemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidlistviewdemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rommelrico.androidlistviewdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val friendsListView = findViewById<ListView>(R.id.friendListView)
+        val myFriends = arrayOf("Mark", "Jane", "Susan", "Jan")
+        var arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myFriends)
+        
     }
 }
