f82796e874d5a9df496303afb2b5a24ab748b637
Rommel Rico <me@rommelrico.com>
2018-11-04 18:03:17 -0800

    Adding a toast

--- app/src/main/java/com/rommelrico/androidlistviewdemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidlistviewdemo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,11 +19,11 @@ class MainActivity : AppCompatActivity() {
         var arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myFriends)
         friendsListView.adapter = arrayAdapter
 
-        friendsListView.setOnItemClickListener(object: AdapterView.OnItemClickListener() {
+        friendsListView.setOnItemClickListener(object: AdapterView.OnItemClickListener {
             override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Toast.makeText(applicationContext, "Hello ${myFriends.get(position)}", Toast.LENGTH_LONG).show();
             }
-
         })
-    }
+
+    } // end onCreate.
 }
